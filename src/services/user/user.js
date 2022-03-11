e9a5330df6a178ca371c0ff8464daa0b032ce159
Doris Nunes <doris90nunes@gmail.com>
2022-03-11 16:02:56 -0300

    feat: adicionado get, update e delete no user.js e importado arquivo de resolvers no server.ts.

--- src/services/user/user.js
+++ src/services/user/user.js
@@ -1,12 +1,25 @@
 const models = require("../../models");
 
 
-const createUser = async (name,email) => {
-    await models.user.create({
-        name,
-        email
-    })
+const createUser = async (data) => {
+    await models.user.create(
+        data,
+    )
     return true;
 }
 
-module.exports = { createUser }
+const getAllUser = async () =>{
+  const result =  await models.user.findAll()
+    return result;
+}
+
+
+const updateUser = async (data) =>{
+    await models.user.update(data, { where : {id:data.id}})
+    return true;
+}
+
+const deleteUser = async (id) => {
+    return await models.user.destroy({where:{id}}) 
+}
+module.exports = { createUser,getAllUser, updateUser, deleteUser }
