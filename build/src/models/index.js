2d5b3acd842bace4ea899a1f981457b509dfafce
Doris Nunes <doris90nunes@gmail.com>
2022-03-14 16:19:26 -0300

    feat:route creation for employees.

--- build/src/models/index.js
+++ build/src/models/index.js
@@ -2,8 +2,10 @@
 const config = require("../config/database/config");
 const { Sequelize } = require("sequelize");
 const User = require("./user");
+const Role = require("./role");
 const sequelize = new Sequelize(config);
 const user = User(sequelize);
+const role = Role(sequelize);
 module.exports = {
-    user
+    user, role
 };
