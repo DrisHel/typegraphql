459b2abe7c90e1b4b6b0883b1462d66e96d77d9e
Doris Nunes <doris90nunes@gmail.com>
2022-03-14 16:20:48 -0300

    feat: build file update.

--- build/src/server.js
+++ build/src/server.js
@@ -3,9 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
 const apollo_server_1 = require("apollo-server");
 const type_graphql_1 = require("type-graphql");
-const UserResolver_1 = require("./graphql/UserResolver");
+const UserResolver_1 = require("./graphql/users/UserResolver");
+const RoleResolver_1 = require("./graphql/roles/RoleResolver");
 const schema = (0, type_graphql_1.buildSchemaSync)({
-    resolvers: [UserResolver_1.UserResolvers]
+    resolvers: [UserResolver_1.UserResolvers, RoleResolver_1.RoleResolvers]
 });
 const server = new apollo_server_1.ApolloServer({
     schema
