b291f1ea4b85322f568ab46c7c01f64f6db25209
Doris Nunes <doris90nunes@gmail.com>
2022-03-14 16:37:47 -0300

    feat : performed the role import.

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,16 @@
 const config = require("../config/database/config");
 const {Sequelize } = require("sequelize");
 const User = require("./user");
+const Role = require("./role")
+
+
 
 
 const sequelize = new Sequelize(config);
 const user = User(sequelize);
+const role = Role(sequelize);
+
 
 module.exports = {
-    user
+    user, role
 }
