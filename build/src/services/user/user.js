bdf71d83eb64fc386c590946e07deb50b1002241
Doris Nunes <doris90nunes@gmail.com>
2022-03-14 16:40:24 -0300

    feat: removed console.log used in testing.

--- build/src/services/user/user.js
+++ build/src/services/user/user.js
@@ -1,10 +1,18 @@
 "use strict";
 const models = require("../../models");
-const createUser = async (name, email) => {
-    await models.user.create({
-        name,
-        email
-    });
+const createUser = async (data) => {
+    await models.user.create(data);
     return true;
 };
-module.exports = { createUser };
+const getAllUser = async () => {
+    const result = await models.user.findAll();
+    return result;
+};
+const updateUser = async (data) => {
+    await models.user.update(data, { where: { id: data.id } });
+    return true;
+};
+const deleteUser = async (id) => {
+    return await models.user.destroy({ where: { id } });
+};
+module.exports = { createUser, getAllUser, updateUser, deleteUser };
